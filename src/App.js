2c9d3b9ae319f9004b140184c7319a529ba58636
leahwilliams1315 <leah1315@gmail.com>
2017-03-23 10:57:50 -0400

    api request and display titles

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+
+const API_KEY = '3e0ff2983607fdcc8476627e4065dfca';
+
+const MyComp = (props) => <h1>{props.data.title}</h1>;
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+        state = {
+            recipes: []
+        };
+
+    componentDidMount(){
+
+        axios.get(`http://food2fork.com/api/search?key=${API_KEY}`)
+            .then(response => {
+                this.setState({recipes: response.data.recipes})
+            });
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <h2>Recipe App</h2>
+                </div>
+
+                {this.state.recipes.map(recipe => <MyComp key={recipe.recipe_id}  data={recipe}/>)}
+
+
+            </div>
+        );
+    }
 }
 
 export default App;
