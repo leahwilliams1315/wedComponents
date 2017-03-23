dabcdb6af7f59d13ed429d9d8a985b5bd682d231
leahwilliams1315 <leah1315@gmail.com>
2017-03-23 12:33:38 -0400

    added link to recipe when image is clicked

--- src/components/recipe_card.js
+++ src/components/recipe_card.js
@@ -4,8 +4,9 @@ import React, { Component } from 'react';
 const RecipeCard = (props) =>
     <div className="recipe-card">
         <h1>{props.data.title}</h1>
+        <a href={props.data.source_url}>
         <img src={props.image} alt="" className="img"/>
-
+        </a>
     </div>;
 
 
